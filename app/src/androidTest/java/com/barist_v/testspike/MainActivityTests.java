8c5c977022c0e1317ed81e6a2beb90562fa1a3f7
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-11-24 12:24:10 -0500

    Add spoon to take screenshots

--- app/src/androidTest/java/com/barist_v/testspike/MainActivityTests.java
+++ app/src/androidTest/java/com/barist_v/testspike/MainActivityTests.java
@@ -1,9 +1,13 @@
 package com.barist_v.testspike;
 
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.filters.MediumTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.jraska.falcon.FalconSpoon;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,13 +21,28 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTests {
 
-
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Before
+    public void setup(){
+    }
+
 
     @Test
-    public void test_activityStarts() {
-        onView(withText("Click me to show toast")).check(matches(isDisplayed()));
+    public void test_onStart_buttonIsVisible() {
+        FalconSpoon.screenshot(mActivityRule.getActivity(), "main-running");
+        onView(withText("Click me to show a dialog")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_onButtonClick_showDialog() {
+        FalconSpoon.screenshot(mActivityRule.getActivity(), "main-beforeClick");
+
+        onView(withText("Click me to show a dialog")).perform(ViewActions.click());
+
+        FalconSpoon.screenshot(mActivityRule.getActivity(), "main-afterClick");
+
+        onView(withText("it works!")).check(matches(isDisplayed()));
     }
 }
